3c1ebc14b17b82be337461c3381f957cbc66770a
computervisionmerl <44785152+computervisionmerl@users.noreply.github.com>
2021-08-04 14:33:32 -0400

    Update needle.py

--- needle.py
+++ needle.py
@@ -1,6 +1,6 @@
 import cv2
 import numpy as np
-from math import atan2, sqrt, sin, cos, degrees
+from math import atan2, degrees
 from helper import euclidean_dist
 
 class Needle:
@@ -30,20 +30,21 @@ class Needle:
     @staticmethod
     def _extend_line_segment(center_line_x : int, needle_line : np.array) -> tuple:
         """
-        Returns the coordinates of the point of intersection ==> Center of the needle
+        Extends the line segment approximating the needle to intersect the vertical mid line of the gauge
+        In case the line segment intersects the mid line already, we can find out the center using hough circles
         """
         x1, y1, x2, y2 = needle_line
 
         if x1 == x2:
-            raise Exception("Needle and center lines are parallel")
+            raise ArithmeticError("Needle and center line are parallel, cannot find intersection")
 
         if y1 == y2:
             return (center_line_x, y1)
 
         ## Based on line equation (y-y1)/(y2-y1) = (x-x1)/(x2-x1)
         y_line_for_x = ((y2 - y1) * (center_line_x - x1) / (x2 - x1)) + y1
-        if y_line_for_x < y1 and y_line_for_x < y2:
-            ## This means that the extrapolation has been done on the wrong side
+        if y_line_for_x < y1 or y_line_for_x < y2:
+            ## This means that the extrapolation has been done on the wrong side or center is within the needle detected
             return None
         else:
             return (center_line_x, int(y_line_for_x))
@@ -63,46 +64,55 @@ class Needle:
                 if length > max_length :
                     max_length = length
                     self.needle = l
-        else:
-            raise ValueError("No hough lines detected !!")
 
+            self.center = self._extend_line_segment(int(self.w/2), self.needle)
+            if self.center is None:
+                self._find_center(image)
+        else:
+            raise ValueError("No hough lines found !")
+
+        return;
     
     def _find_center(self, image : np.array) -> None:
+        """
+        The circle closest to the center of the image is assumed to be the center of the needle
+        which is true in most gauges
+        """
         circles = cv2.HoughCircles(image, cv2.HOUGH_GRADIENT, self.dp, self.minDist, None, self.param1, self.param2, self.minRadius, self.maxRadius)        
         min_dist = 1e20
         center = (image.shape[0]/2, image.shape[1]/2)
         if circles is not None:
             circles = np.uint16(np.around(circles))
             for i in circles[0,:]:
-                dist = sqrt((i[0]-center[0])**2 + (i[1]-center[1])**2)
+                dist = euclidean_dist((i[0],i[1]), center)
                 if dist < min_dist:
                     min_dist = dist
                     self.center = (i[0], i[1])
         return;
 
     def _find_quadrant(self, point : tuple) -> int:
-        ## The quadrant is found based on pixel coordinate system with center of needle as origin
-        ## First quadrant --> x(+ve), y(-ve)
+        """
+        The quadrant is found based on pixel coordinate system with center of needle as origin
+        First quadrant --> x(+ve), y(-ve)
+        Second quadrant --> x(-ve), y(-ve)
+        Third quadrant --> x(-ve), y(+ve)
+        Fourth quadrant --> x(+ve), y(+ve)
+        """
         if point[0] >= self.center[0] and point[1] < self.center[1]:
             return 1
-        
-        ## Second quadrant --> x(-ve), y(-ve)
         elif point[0] < self.center[0] and point[1] <= self.center[1]:
             return 2
-        
-        ## Third quadrant --> x(-ve), y(+ve)
         elif point[0] <= self.center[0] and point[1] > self.center[1]:
             return 3
-
-        ## Fourth quadrant --> x(+ve), y(+ve)
         elif point[0] > self.center[0] and point[1] >= self.center[1]:
             return 4
-
         else:
             raise ValueError("Quadrant is not identified")
 
     def _find_angle_based_on_quadrant(self, quad : int, point : tuple) -> float:
-        ## Gives the angle of the line joining the point to the center of the needle, with respect to the negative y-axis
+        """
+        Gives the angle of the line joining the point to the center of the needle, with respect to the downward y-axis
+        """
         slope = degrees(atan2(abs(point[1] - self.center[1]), abs(point[0] - self.center[0])))
         if quad == 1:
             return 270 - slope
@@ -123,6 +133,7 @@ class Needle:
         if self.center is not None:
             cv2.circle(image, (self.center[0], self.center[1]), 5, (255,0,0), -1, cv2.LINE_AA)
 
+        cv2.line(image, (int(self.w/2), 0), (int(self.w/2), self.h), (0,0,0), 2, cv2.LINE_AA)
         cv2.imshow("image", image)
         cv2.waitKey(0)
         return;
